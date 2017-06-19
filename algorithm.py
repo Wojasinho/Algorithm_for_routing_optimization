d1a8ec58152b4ec4a2c05f440b5eb699ab178cd3
root <root@IBM.(none)>
2017-06-19 10:15:51 +0200

    Comments + docstring, + topology of connection our company between departments,
    Cosmetic works

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,10 @@
 class Optimizing(object):
+    """Object from these class can find shortest (fastest way) between Cities. It can be usefull for optimizing network 
+    routing. For defined topology, we can find best connection ex. City A -> City B. It's a script for extra exercise
+    for Student Project 'Desing and Implement Network for company with 6 department and within 1  Central Department
     
+    """
+
     def __init__(self, graph_dict=None):
 
         if graph_dict == None:
@@ -9,8 +14,9 @@ class Optimizing(object):
 
 
     def find_all_paths(self, cityA, cityB, path=[]):
-        """ find all paths from cityA to 
-            cityB in graph """
+
+        """ Find all paths from cityA to cityB in defined topology """
+
         self.cityA=cityA
         self.cityB=cityB
 
@@ -28,28 +34,32 @@ class Optimizing(object):
                 for p in extended_paths:
                     paths.append(p)
 
-        # print min(paths, key=len)
+
         self.bestpaths=paths
         return self.bestpaths
-        # return paths
+
 
 
     def choose_path(self):
+        """Choose shortest path from all paths."""
         self.fastestpath= min(self.bestpaths, key=len)
         print (self.fastestpath)
 
     def count_delay(self):
+        """Count delay, it depends how many cities are between our start and destination city """
 
         total_delay=0
         generated_network_traffic=0
         channel_bandwidth=196000
 
+        # Generated traffic from Central Department in MB (ex. From Jelenia Gora -> Gniezno)
         if self.fastestpath[0]=='Jelenia':
             generated_network_traffic = 200
 
+        # Generated traffic to Central Department in MB (ex. From Bydgoszcz -> Jelenia Gora)
         if self.fastestpath[-1] == 'Jelenia':
             generated_network_traffic = 250
-
+        # Genereted traffic between two Department in MB (ex. From Wroclaw -> Legnica)
         if not (self.fastestpath[0]=='Jelenia' or self.fastestpath[-1] == 'Jelenia'):
             generated_network_traffic=40
 
@@ -61,13 +71,8 @@ class Optimizing(object):
         coefficient_delay = (flow /(channel_bandwidth-flow))
         total_delay = ((1/total_packets)*coefficient_delay)
 
-        print ("Najkrótsze połączenie, opóźnienie na trasie wynosi" + str(round(total_delay,5)))
-
-
-
-
-
-
+        # print ("Najkrótsze połączenie, opóźnienie na trasie wynosi : " + str(round(total_delay,5) ))
+        print("Shortest/Fastest connection, delay is equal " + str(round(total_delay, 5)))
 
 
 cities = {
@@ -82,26 +87,11 @@ cities = {
 
 
 
-
-cityA="Poznan"
-cityB="Wroclaw"
+# Defined here interested city
+cityA="Wroclaw"
+cityB="Poznan"
 
 find_connection = Optimizing(cities)
 find_connection.find_all_paths(cityA, cityB)
 find_connection.choose_path()
 find_connection.count_delay()
-
-# find_connection.choose_path()
-# print("Path from " + cityA + " to " + cityB + " :" )
-# a = min(bestpaths, key=len)
-# print (a)
-
-# print(find_connection)
-
-# print("""A path from "P" to "J":""")
-# print(graph.find_path("P", "J"))
-
-
-
-
-# graph.choose_path()
